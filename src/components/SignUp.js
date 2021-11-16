2bcd14734cf977fa12163acfc76e889c4b1dd18d
kiana2001 <kianakermani1381@gmail.com>
2021-11-16 12:27:50 +0330

    styling components

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {validate} from "./validate.js";
+import styles from './SignUp.module.css';
 
 import {notify} from "./toast"
   import { ToastContainer} from 'react-toastify';
@@ -38,7 +39,7 @@ const submitHandler = event =>{
     event.preventDefault();
     notify();
     if(!Object.keys(errors).length){
-        notify("you have sign successfully")
+        notify("you have sign up successfully")
     }else{
         notify("you are not signed in")
         setTouched({
@@ -51,35 +52,41 @@ const submitHandler = event =>{
     }
 }
     return (
-        <div>
-        <form onSubmit={submitHandler}>
-            <h2>Sign Up</h2>
-            <div>
-                <label>name</label>
-                <input type="text" name="name" value={data.name} onChange={changeHandler} onFocus={focusHandler}/>
+        <div className={styles.container}>
+        <form onSubmit={submitHandler} className={styles.formContainer}>
+            <h2 className={styles.header}>Sign Up</h2>
+            <div className={styles.formField}>
+                <label >name</label>
+                <input className={(errors.name && touched.name ) ? styles.uncompleted : styles.formInput }type="text" name="name" value={data.name} onChange={changeHandler} onFocus={focusHandler}/>
                 {errors.name && touched.name && <span>{errors.name}</span>} 
             </div>
-            <div>
+            <div className={styles.formField}>
                 <label>email</label>
-                <input type="text" name="email" value={data.email} onChange={changeHandler}onFocus={focusHandler}/>
+                <input  className={(errors.email && touched.email ) ? styles.uncompleted : styles.formInput }
+                type="text" name="email" value={data.email} onChange={changeHandler}onFocus={focusHandler}/>
                 {errors.email  && touched.email&& <span>{errors.email}</span>} 
             </div>
-            <div>
+            <div className={styles.formField}>
                 <label>Password</label>
-                <input type="password" name="password" value={data.password} onChange={changeHandler} onFocus={focusHandler}/>
+                <input className={(errors.password && touched.password ) ? styles.uncompleted : styles.formInput }
+                 type="password" name="password" value={data.password} onChange={changeHandler} onFocus={focusHandler}/>
                 {errors.password && touched.password && <span>{errors.password}</span>}  
             </div>
-            <div>
+            <div className={styles.formField}>
                 <label>confirm password</label>
-                <input type="password" name="confirmpassword " value={data.confirmPassword} onChange={changeHandler} onFocus={focusHandler}  />
+                <input className={(errors.confirmPassword && touched.confirmPassword ) ? styles.uncompleted : styles.formInput }
+                type="password" name="confirmpassword " value={data.confirmPassword} onChange={changeHandler} onFocus={focusHandler}  />
                 {errors.confirmPassword  && touched.confirmPassword&& <span>{errors.confirmPassword}</span>} 
             </div>
-            <div>
-                <label>i accept</label>
-                <input type="checkbox" name="isAccepted" value={data.isAccepted} />
+            <div className={styles.formField}> 
+            <div className= {styles.checkboxContainer}>
+                <label >i accept</label>
+                <input 
+                 type="checkbox" name="isAccepted" value={data.isAccepted} />
+                 </div>
                 {errors.isAccepted && <span>{errors.isAccepted}</span>} 
             </div>
-            <div>
+            <div className={styles.formButtons}>
                 <a href="KJFKF" >login</a>
                 <button type="submit" >sign up</button>
             
